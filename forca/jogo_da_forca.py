516396eebbbafa158b856da752fda44118280dbf
cyrogabrielcc <cyrocunhacg@gmail.com>
2022-01-07 17:42:31 -0300

    org

---
+++ forca/jogo_da_forca.py
@@ -0,0 +1,31 @@
+import random
+
+def hangman():
+   word = random.choice(["pugger" ,  "littlepugger" , "tiger" , "superman" , "thor" , "pokemon" , "avengers" , "savewater" , "earth" , "annable" ])
+   validletters = 'abcdefghijklmnopqrstuvxwyzç'
+   print(word)
+   guessmade = ''
+    
+
+
+
+
+
+
+
+
+
+
+
+
+name = input("Entre com seu nome: ")
+print ("Seja bem vindo, ", name, "!")
+
+print("--------------------------")
+
+print("Descubra a palavra em 10 tentativas: \n")
+hangman()
+
+
+
+
--- readme.md
+++ readme.md
@@ -1,8 +1,9 @@
-#Dicionário
+# Dicionário
 
 Mini dicionário que utiliza para sugerir palavras quando houver erro de escrita: 
+-> difflib import get_close_matches
 
-difflib import get_close_matches
+# Simulador de dado de RPG
 
 
 
