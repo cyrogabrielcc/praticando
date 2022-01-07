eb001441d8cd043966824c9cad9a1a09515c7a70
cyrogabrielcc <cyrocunhacg@gmail.com>
2022-01-07 16:26:53 -0300

    Subindo simulador de dado

---
+++ simulatior.py
@@ -0,0 +1,57 @@
+import random
+
+
+
+y = "y"
+
+while y == "y":
+
+    x = random.randint(1,6)
+
+    if x == 1:
+        print("---------")
+        print("|       |")
+        print("|   0   |")
+        print("|       |")
+        print("---------")
+
+    if x == 2:
+        print("---------")
+        print("|       |")
+        print("| 0   0 |")
+        print("|       |")
+        print("---------") 
+
+    if x == 3:
+        print("---------")
+        print("|   0   |")
+        print("|   0   |")
+        print("|   0   |")
+        print("---------") 
+
+    if x == 4:
+        print("---------")
+        print("|0     0|")
+        print("|       |")
+        print("|0     0|")
+        print("---------") 
+
+    if x == 5:
+        print("---------")
+        print("|0     0|")
+        print("|   0   |")
+        print("|0     0|")
+        print("---------") 
+
+    if x == 6:
+        print("---------")
+        print("|0     0|")
+        print("|0     0|")
+        print("|0     0|")
+        print("---------") 
+
+    y = input ("y or n: \n")
+
+
+
+
